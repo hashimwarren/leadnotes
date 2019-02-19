e174f28dc950a4ca359e32e92eb79166397ac8a6
Hashim Warren <hashimwarren@gmail.com>
2019-02-18 20:58:24 -0500

    The Handlebars Middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const exphbs  = require('express-handlebars')
 
-//Handlebars Middleware
+//the Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
