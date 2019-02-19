5212367a7d7f7bbc7f5620bc1e3bb0c46e3dc56e
Hashim Warren <hashimwarren@gmail.com>
2019-02-18 20:56:07 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const exphbs  = require('express-handlebars')
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//Index route
+app.get('/', (req, res) => {
+
+    res.send('INDEX!')
+})
+
+// About route
+app.get('/about', (req, res) => {
+    res.send('All about King Hashim!')
+})
+
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server started on ${port}`)
+})
