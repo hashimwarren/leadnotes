e3bfaf3ddb053473471b5ec9469e7280f85a9920
Hashim Warren <hashimwarren@gmail.com>
2019-02-24 17:47:24 -0500

    Made changes to match Brad's code

--- app.js
+++ app.js
@@ -1,80 +1,83 @@
-const express = require('express')
-const exphbs  = require('express-handlebars')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const app = express();
 
-const app = express()
-
-//Map global promise - get rid of warning
-mongoose.Promise = global.Promise
-
-//Connect to Mongoose
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
 mongoose.connect('mongodb://localhost/leadnotes-dev', {
-    useMongoClient: true
+  useMongoClient: true
 })
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err))
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-//Load Leads model
-require('./models/Lead')
-const Lead = mongoose.model('leads')
+// Load Idea Model
+require('./models/Lead');
+const Lead = mongoose.model('leads');
 
-
-//the Handlebars Middleware
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'}));
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-//Body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-//Index route
+// Index Route
 app.get('/', (req, res) => {
-    const title = 'Welcome Alex!'
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
 
-    res.render('index', {
-        title: title
-    })
-})
-
-// About route
+// About Route
 app.get('/about', (req, res) => {
-    res.render('about')
-})
+  res.render('about');
+});
 
-//Add Notes form
+// Add Idea Form
 app.get('/notes/add', (req, res) => {
-    res.render('notes/add')
-})
+  res.render('notes/add');
+});
 
-//Process form
+// Process Form
 app.post('/notes', (req, res) => {
-    let errors = []
-    if (!req.body.name) {
-        errors.push({text: 'Please add a name'})
-    }
-    if (!req.body.note) {
-        errors.push({text: 'Please add a note'})
-    }
-    if (errors.length > 0) {
-        res.render('notes/add', {
-            errors: errors,
-            name: req.body.name,
-            note: req.body.note
-        })
-    }
-    else {
-        res.send('passed')
+  let errors = [];
 
+  if(!req.body.name){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.notes){
+    errors.push({text:'Please add some details'});
+  }
+
+  if(errors.length > 0){
+    res.render('notes/add', {
+      errors: errors,
+      name: req.body.name,
+      notes: req.body.notes
+    });
+  } else {
+    const newUser = {
+      name: req.body.name,
+      notes: req.body.notes
     }
-    
-})
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/notes');
+      })
+  }
+});
 
+const port = 5070;
 
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Server started on ${port}`)
-})
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
+});
